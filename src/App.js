86f758a9e57bc28c158a9fccddb0bd44417eaa0c
Roshan Kumar Sharma <roshan.sharma@deprontoinfotech.com>
2023-09-02 17:24:59 +0530

    Added Blog Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css"
-import Home from "./Component/Home/Home"
 import LandingPage from "./Component/LandingPage/landingPage"
 import Login from "./Component/Login/Login"
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./Component/Navbar/Navbar"
+import Blogs from "./Component/Blogs/Blogs"
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/Blogs" element={<Blogs />} />
       </Routes>
     </div>
   )
---
+++ src/Component/Blogs/Blogs.jsx
@@ -0,0 +1,15 @@
+import "./Blog.css"
+import React from 'react'
+const Blogs = () => {
+  return (
+    <div className="blogContainer">
+      <div className="blogMain">
+          <div className="blogHeader">
+            <p><span>Learn</span> <span>More</span></p>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Blogs;
