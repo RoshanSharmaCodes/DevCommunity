db2816cfd9158afb782a526b48a0c410dd386887
Roshan Kumar Sharma <roshan.sharma@deprontoinfotech.com>
2023-09-03 00:45:56 +0530

    Blogs Section Added

---
+++ src/Component/Blogs/BlogCard/BlogCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./BlogCard.css"
+import { Card } from 'antd'
+
+const BlogCard = () => {
+  return (
+    <Card title="Top 10 JavaScript Resources" bordered={true} className='blogCard'>
+    
+    </Card>
+  )
+}
+
+export default BlogCard;
--- src/Component/Blogs/Blogs.jsx
+++ src/Component/Blogs/Blogs.jsx
@@ -1,15 +1,31 @@
 import "./Blog.css"
-import React from 'react'
+import React from "react"
+import BlogCard from "./BlogCard/BlogCard"
+import { Input } from "antd";
+const { Search } = Input;
 const Blogs = () => {
   return (
     <div className="blogContainer">
       <div className="blogMain">
-          <div className="blogHeader">
-            <p><span>Learn</span> <span>More</span></p>
-          </div>
+        <div className="blogHeader">
+          <p>
+            <span>Learn</span> <span>More</span>
+          </p>
+          <div className="blueDash"></div>
+        </div>
+        <Search placeholder="Best JavaScript Resources" allowClear class="blogSearchBar" style={{width:"500px",marginBottom:"30px"}} size="large" onSearch={""} enterButton />
+        <div className="blogBody">
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+        </div>
       </div>
     </div>
   )
 }
 
-export default Blogs;
+export default Blogs
