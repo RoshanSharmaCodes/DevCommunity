eee488e1113df3cce183ff03047ee72499017d33
Rishabh Mishra <rishabhmmishra@gmail.com>
2023-09-04 21:46:46 +0530

    signup page work

--- src/Component/SignUp/SignUpCard/SignUpCard.jsx
+++ src/Component/SignUp/SignUpCard/SignUpCard.jsx
@@ -5,7 +5,23 @@ const SignUpCard = () => {
   return (
     <div className="signupCardContainer">
       <div className="signupCardMain">
-        
+        <div className="signupDesign">
+          <img className='interview' alt="interviewimage" src="assets/images/interview.svg" />
+        </div>
+        <div className="signupArea">
+          <div className="navbar">
+            <span>Sign Up</span>
+            <div className="toggle">
+              <label class="switch">
+                <input type="checkbox" />
+                <span class="slider round"></span>
+              </label>
+            </div>  
+          </div>
+          <div className="signupForm">
+            
+          </div>
+        </div>   
       </div>
     </div>
   )
