0ed109658db3ba4431f6e994d38f964e317e7afd
Rishabh Mishra <rishabhmmishra@gmail.com>
2023-09-05 17:27:44 +0530

    signup page done

--- src/Component/SignUp/SignUpCard/SignUpCard.jsx
+++ src/Component/SignUp/SignUpCard/SignUpCard.jsx
@@ -1,9 +1,14 @@
 import React from "react"
 import "./SignUpCard.css"
-import SignUpMentor from "./SignUpMentor/SignUpMentor"
-import SignUpCandidate from "./SignUpCandidate/SignUpCandidate"
+import { Button, Form, Input, Divider, Select } from "antd"
+import {FaGithubSquare, FaGooglePlusSquare, FaTwitterSquare} from "react-icons/fa"
 
 const SignUpCard = () => {
+
+    const FormSubmit = () => {
+      alert("Form Submitted")
+    }
+
   return (
     <div className="signupCardContainer">
       <div className="signupCardMain">
@@ -11,17 +16,106 @@ const SignUpCard = () => {
           <img className='interview' alt="interviewimage" src="assets/images/interview.svg" />
         </div>
         <div className="signupArea">
-          <div className="navbar">
-            <span>Sign Up</span>
-            <div className="toggle">
-              <label class="switch">
-                <input type="checkbox" />
-                <span class="slider round"></span>
-              </label>
-            </div>  
-          </div>
           <div className="signupForm">
-            <SignUpMentor />
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              width: 400,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={FormSubmit}
+            onFinishFailed={FormSubmit}
+            autoComplete="off"
+          >
+
+            <Form.Item 
+              name="position" 
+              label="" 
+              rules={[
+                {
+                  required: true,
+                  message: "Select your Position!",
+                },
+              ]}
+              style={{width:600}}
+              hasFeedback
+              >
+              <Select placeholder="Join as">
+                <Select.Option value='mentor'>Mentor</Select.Option>
+                <Select.Option value='candidate'>Candidate</Select.Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item
+              label=""
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please type your username!",
+                },
+              ]}
+              style={{width:600}}
+              hasFeedback
+            >
+              <Input placeholder="Enter Username..."  />
+            </Form.Item>
+
+
+            <Form.Item 
+            name="password" 
+            label=""
+            rules={[
+                {
+                  required: true,
+                  message: "Please type Password!",
+                  min: 8,
+                },
+              ]}
+              style={{width:600}}
+              hasFeedback
+            >
+            <Input.Password placeholder='Type your password'/>
+        </Form.Item>
+
+        <Form.Item 
+            name="confirmPassword" 
+            label=""
+            rules={[
+                {
+                  required: true,
+                  message: "Confirm your username!",
+                  min: 8,
+                },
+              ]}
+              style={{width:600}}
+              hasFeedback
+              >
+            <Input.Password block placeholder='Confirm your password'/>
+        </Form.Item>
+
+            <Form.Item
+              style={{width:600}}
+            >
+              <Button block type="primary" htmlType="submit" >
+                Sign Up
+              </Button>
+            </Form.Item>
+          </Form>
+          <Divider>Or</Divider>
+          <div className="signupOptions">
+              <FaGooglePlusSquare className="signupIcons"/>
+              <FaGithubSquare className="signupIcons"/>
+              <FaTwitterSquare className="signupIcons"/>
+          </div>
           </div>
         </div>   
       </div>
